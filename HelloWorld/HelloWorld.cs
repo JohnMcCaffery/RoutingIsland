7b791419692f0864953edf6ef5185d1a434e50f9
= <jm726@st-andrews.ac.uk>
2012-10-26 13:27:48 +0100

    Added license files.

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿//MRM:C#
+//MRM:X -a XMRM/HelloWorld.dll -c Start.HelloWorld
+using OpenSim.Region.OptionalModules.Scripting.Minimodule;
+using System;
+
+namespace Start {
+    public class HelloWorld : MRMBase {
+        public override void Start(string[] args) {
+            Host.Object.Say("Hello World!");
+        }
+
+        public override void Stop() { }
+    }
+}
